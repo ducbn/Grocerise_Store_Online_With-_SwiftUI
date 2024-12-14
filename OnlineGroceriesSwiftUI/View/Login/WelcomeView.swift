70c8aec635d16ddd337c525e0c168a631beff25f
BuiNgocDuc <ducbui27924@gmail.com>
2024-12-14 11:25:55 +0700

    set up project

---
+++ OnlineGroceriesSwiftUI/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by Bùi ngọc Đức on 13/12/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
