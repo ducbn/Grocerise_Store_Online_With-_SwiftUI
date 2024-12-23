3f28ca51c7db7110fdf96f357843ded556ae23d4
Bui Ngoc Duc <buingocduc@MacBook-Air-cua-Bui.local>
2024-12-23 15:44:28 +0700

    finish signIN view

---
+++ OnlineGroceriesSwiftUI/UICommon/CountryPicker.swift
@@ -0,0 +1,39 @@
+//
+//  CountryPicker.swift
+//  OnlineGroceriesSwiftUI
+//
+//  Created by Bui Ngoc Duc on 20/12/24.
+//
+
+import SwiftUI
+import CountryPicker
+struct CountryPickerUI: UIViewControllerRepresentable {
+    @Binding var country: Country?
+    class Coordinator: NSObject, CountryPickerDelegate {
+        var parent: CountryPickerUI
+        
+        init(_ parent: CountryPickerUI) {
+            self.parent = parent
+        }
+        
+        func countryPicker(didSelect country: Country) {
+            parent.country = country
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let countryPicker = CountryPickerViewController()
+        countryPicker.selectedCountry = "IN"
+        countryPicker.delegate = context.coordinator
+        
+        return countryPicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
--- OnlineGroceriesSwiftUI/View/Login/WelcomeView.swift
+++ OnlineGroceriesSwiftUI/View/Login/WelcomeView.swift
@@ -2,17 +2,55 @@
 //  WelcomeView.swift
 //  OnlineGroceriesSwiftUI
 //
-//  Created by Bùi ngọc Đức on 13/12/24.
+//  Created by Bùi Ngọc Đức on 13/12/24.
 //
 
 import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("welcom_bg")
+                .resizable()
+                .scaledToFit()
+                .frame(width: .screenWidth, height: .screenHeight)
+            VStack{
+                Spacer()
+                Text("Welcome\nto our store")
+                    .font(.customfont(.semibold, fontSize: 48))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("Giao hàng nhanh chóng, chất lượng đạt chuẩn")
+                    .font(.customfont(.medium, fontSize: 16))
+                    .foregroundColor(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 30)
+                
+                NavigationLink {
+                    SignInView()
+                } label: {
+                    RoundButton(title: "Bắt Đầu") {
+                        
+                    }
+                }
+                
+                Spacer()
+                    .frame(height: 80)
+            }
+            .padding(.horizontal, 20)
+
+        }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea()
     }
-}
+} 
 
 #Preview {
-    WelcomeView()
+    NavigationView{
+        WelcomeView()
+    }
+    
 }
